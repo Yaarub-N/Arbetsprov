57ed5dffd89c8c0fe1cb94cbe89efd60b7c8627a
Yaarub Nassr <yaarub.nasser@yahoo.com>
2025-06-09 01:13:03 +0200

    add unit tests and configure test project Includes tests for FileService, RequestService, NumberService,

---
+++ GetNextNumber.Tests/FileService_Tests.cs
@@ -0,0 +1,26 @@
+﻿using GetNextNumber.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GetNextNumber.Tests
+{
+    public class FileService_Tests
+    {
+        [Fact]
+        public async Task SaveToFileAsync_WritesCorrectly()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test.txt");
+            var fileService = new FileService();
+            var lines = new[] { "Test","Test2" };
+
+            var result = await fileService.SaveToFileAsync(lines, path);
+
+            Assert.True(result.Success);
+            Assert.Equal(200, result.StatusCode);
+            
+            File.Delete(path); 
+        }
+    }
+}
---
+++ GetNextNumber.Tests/NumberListService_Tests.cs
@@ -0,0 +1,39 @@
+﻿using GetNextNumber.Services;
+
+
+namespace GetNextNumber.Tests
+{
+    public class NumberListService_Tests
+    {
+        private readonly NumberListService _service;
+
+        public NumberListService_Tests()
+        {
+            _service = new NumberListService();
+        }
+
+        [Fact]
+        public void GenerateNumberList__Should_ReturnExpected_Values()
+        {
+            int start = 1;
+            int count = 15;
+
+            var result = _service.GenerateNumberList(start, count).ToList();
+
+            Assert.Equal("1", result[0]);
+            Assert.Equal("5 FemMultipel", result[4]);
+            Assert.Equal("15 TreMultipelFemMultipel", result[14]);
+        }
+
+        [Theory]
+        [InlineData(3, "3 TreMultipel")]
+        [InlineData(5, "5 FemMultipel")]
+        [InlineData(15, "15 TreMultipelFemMultipel")]
+        [InlineData(7, "7")]
+        public void Handles_Multiples_Correctly(int input, string expected)
+        {
+            var result = _service.GenerateNumberList(input, 1).First();
+            Assert.Equal(expected, result);
+        }
+    }
+}
